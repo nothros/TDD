18f97e5f5b30465bc95560ce6ab39cd3c0c9cd19
Erika Horros-Wallin <erika.horros@gmail.com>
2023-01-28 17:22:18 +0200

    Add parallel attribute date2 to first method which use date

--- tdd-mooc-small-steps/src/prices.mjs
+++ tdd-mooc-small-steps/src/prices.mjs
@@ -20,8 +20,8 @@ function createApp(database) {
     const type = req.query.type;
     const baseCost = database.findBasePriceByType(type).cost;
     const date = parseDate(req.query.date);
-    const date2 = parseDate2(req.query.date);
-    const cost = calculateCost(age, type, date, baseCost);
+    const date2 = parseDate2(req.query.date)
+    const cost = calculateCost(age, type, date, baseCost, date2);
     res.json({ cost });
   });
 
@@ -37,7 +37,7 @@ function createApp(database) {
     }
   }
 
-  function calculateCost(age, type, date, baseCost) {
+  function calculateCost(age, type, date, baseCost, date2) {
     if (type === "night") {
       return calculateCostForNightTicket(age, baseCost);
     } else {
