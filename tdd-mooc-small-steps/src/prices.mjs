68cce6ca2723ab39b2d70d98fc9294a3436dcd84
Erika Horros-Wallin <erika.horros@gmail.com>
2023-01-28 17:18:22 +0200

    Add date2 which use plaindateconverter

--- tdd-mooc-small-steps/src/prices.mjs
+++ tdd-mooc-small-steps/src/prices.mjs
@@ -20,6 +20,7 @@ function createApp(database) {
     const type = req.query.type;
     const baseCost = database.findBasePriceByType(type).cost;
     const date = parseDate(req.query.date);
+    const date2 = parseDate2(req.query.date);
     const cost = calculateCost(age, type, date, baseCost);
     res.json({ cost });
   });
