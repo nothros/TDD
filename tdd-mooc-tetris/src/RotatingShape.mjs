6f21bdca33c288b1c75a3a3e4cbed8693416d4cc
Erika Horros-Wallin <erika.horros@gmail.com>
2023-01-29 20:32:12 +0200

    Add matrix and printing

--- tdd-mooc-tetris/src/RotatingShape.mjs
+++ tdd-mooc-tetris/src/RotatingShape.mjs
@@ -1,14 +1,35 @@
 
 export class RotatingShape{
-    shape;
-
+    
+    rows;
+    cols;
+    matrix;
     constructor(shape){
-        this.shape = shape.replaceAll(" ", "")+"\n";
-        console.log(this.shape);
-
+        this.matrix = [];
+        this.rows = shape.replaceAll(" ", "").split("\n")
+        
+        for(var i = 0; i < this.rows.length; i++){
+            this.matrix.push([...this.rows[i]]);
+        }
     }
 
     toString(){
-        return this.shape;
+        return this.print();
     }
+
+    print(){
+        var s='';
+        for(var row = 0; row < this.matrix.length; row++){
+            for(var col = 0; col < this.matrix[0].length; col++){
+                s+=this.matrix[row][col]
+            }
+            s+="\n"
+        }
+        return s;
+    }
+
+    rotateRight(){
+        null;
+    }
+
 }
